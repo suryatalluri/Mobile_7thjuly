73e396a302a4a92971a2a89c249d87fb21d90293
Surya null <surya.talluri+12@wavemaker.com>
2016-07-07 06:50:07 +0000

    New files added - [glyphicons-halflings-regular.svg,User.java,hrdb_procedure.json,Roboto-Regular-webfont.woff,DepartmentService.java,DepartmentServiceImpl.java,UserService.java,UserServiceImpl.java,hrdb-queries.hbm.xml,style.less,VacationController.java,FileController.java,VacationDao.java,style.css,login11.js,style.css,glyphicons-halflings-regular.woff,glyphicons-halflings-regular.woff2,wavicon.woff,variables.less,style.less,hrdb-procedures.mappings.json,DepartmentDao.java,glyphicons-halflings-regular.woff2,ProcedureExecutionController.java,db-connection-settings.json,servicedef.xml,VacationServiceImpl.java,HrdbProcedureExecutorServiceImpl.java,Roboto-Regular-webfont.ttf,login11.variables.json,Vacation.java,glyphicons-halflings-regular.ttf,EmployeeServiceImpl.java,glyphicons-halflings-regular.woff,FileService.java,Roboto-Regular.ttf,Department.java,HrdbQueryExecutorService.java,servicedef.xml,DepartmentController.java,hrdb.properties,Employee.java,hrdb_published_dataModel.json,package-info.java,login11.html,README.md,hrdb_query.json,hrdb.spring.xml,UserController.java,HrdbProcedureExecutorService.java,EmployeeDao.java,glyphicons-halflings-regular.eot,Roboto-Regular-webfont.svg,ReadMe.txt,FileService.spring.xml,hrdb.script,wavicon.ttf,QueryExecutionController.java,login11.css,theme.png,glyphicons-halflings-regular.eot,glyphicons-halflings-regular.svg,Roboto-Regular-webfont.eot,.wmproject.properties,glyphicons-halflings-regular.ttf,EmployeeService.java,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,UserDao.java,EmployeeController.java,login11,android] Files modified - [Main.html,pages-config.json,project-services.xml,config.xml,.wmproject.properties,wmProperties.js,app.variables.json,Main.variables.json,production.properties,config.json,pom.xml,index.html,development.properties] Refactored files - [] Fixed bugs - [] Removed files - [glyphicons-halflings-regular.woff,style.less,variables.less,ReadMe.txt,glyphicons-halflings-regular.svg,glyphicons-halflings-regular.woff2,style.less,wavicon.woff,Roboto-Regular-webfont.eot,.wmproject.properties,glyphicons-halflings-regular.eot,glyphicons-halflings-regular.woff2,glyphicons-halflings-regular.ttf,wavicon.ttf,Roboto-Regular-webfont.svg,glyphicons-halflings-regular.woff,glyphicons-halflings-regular.ttf,glyphicons-halflings-regular.eot,Roboto-Regular-webfont.woff,style.css,Roboto-Regular-webfont.ttf,glyphicons-halflings-regular.svg,theme.png,style.css,Roboto-Regular.ttf,ReadMe.txt]

---
+++ src/main/webapp/pages/login11/login11.js
@@ -0,0 +1,11 @@
+Application.$controller("login11PageController", ["$scope", function ($scope) {
+    "use strict";
+}]);
+
+
+Application.$controller("login1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,5 +1,5 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "mobile",
+  "activeTheme" : "green-day",
   "defaultLanguage" : "en",
   "displayName" : "Mobile_7thjuly",
   "homePage" : "Main",
